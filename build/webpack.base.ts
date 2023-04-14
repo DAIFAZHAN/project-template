d550e5cd59cb0870ada4a096c0dab39f0aebd7c2
daifzh <daifzh@163.com>
2023-04-15 02:39:56 +0800

    完善 webpack.base.ts。安装html-webpack-plugin

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -1,4 +1,6 @@
-import { Configuration } from 'webpack';
+import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
 const path = require("path");
 
 const baseConfig: Configuration = {
@@ -12,11 +14,55 @@ const baseConfig: Configuration = {
   },
   // loader 配置
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: {
+          loader: "babel-loader",
+          options: {
+            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  // 设置兼容目标浏览器版本,也可以在根目录配置.browserslistrc文件,babel-loader会自动寻找上面配置好的文件.browserslistrc
+                  targets: {
+                    browsers: ["> 1%", "last 2 versions", "not ie <= 8"],
+                  },
+                  useBuiltIns: "usage", // 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
+                  corejs: 3, // 配置使用core-js使用的版本
+                  loose: true,
+                },
+              ],
+              // 如果您使用的是 Babel 和 React 17，您可能需要将 "runtime": "automatic" 添加到配置中。
+              // 否则可能会出现错误：Uncaught ReferenceError: React is not defined
+              ["@babel/preset-react", { runtime: "automatic" }],
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /.css$/, //匹配 css 文件
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
-  // plugins 的配置
-  plugins: []
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
+      template: path.join(__dirname, "../public/index.html"),
+      // 压缩html资源
+      minify: {
+        collapseWhitespace: true, //去空格
+        removeComments: true, // 去注释
+      },
+    }),
+  ],
 };
+
+export default baseConfig;
